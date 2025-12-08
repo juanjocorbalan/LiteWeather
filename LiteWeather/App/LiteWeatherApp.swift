818110c287fa7633dabaa9c82add7ce23529b6c7
Juanjo Corbalán <juanjocorbalan@gmail.com>
2025-12-08 19:39:42 +0100

    App: Added basic infrastructure

---
+++ LiteWeather/App/LiteWeatherApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct LiteWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# LiteWeather iOS App
+
+LiteWeather is an iOS application that displays weather information using the OpenWeatherMap API.
