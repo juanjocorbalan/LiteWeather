02e428c805274c270a90ecd96deb1575ab075dac
Juanjo Corbalán <juanjocorbalan@gmail.com>
2025-12-08 18:23:41 +0100

    Initial Commit

---
+++ LiteWeather/LiteWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiteWeatherApp.swift
+//  LiteWeather
+//
+//  Created by Juanjo Corbalan on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LiteWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LiteWeatherTests/LiteWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiteWeatherTests.swift
+//  LiteWeatherTests
+//
+//  Created by Juanjo Corbalan on 8/12/25.
+//
+
+import Testing
+@testable import LiteWeather
+
+struct LiteWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
